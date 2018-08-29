80ad1ecd4ac53cab609fd00479d5c2b47a3832ac
Kristoffer Risa <kristofferrisa@gmail.com>
2018-08-29 22:04:36 +0200

    Added application insights

--- src/Program.cs
+++ src/Program.cs
@@ -19,6 +19,7 @@ namespace Convert.Express
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
