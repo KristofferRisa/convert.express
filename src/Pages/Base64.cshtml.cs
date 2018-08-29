3f3aec8c8b70e512c3825b81f5397e08a5ab3b02
Kristoffer Risa <kristofferrisa@gmail.com>
2018-08-29 23:07:06 +0200

    Exception handling for base64 decoding

--- src/Pages/Base64.cshtml.cs
+++ src/Pages/Base64.cshtml.cs
@@ -52,8 +52,15 @@ namespace Convert.Express.Pages
 
         public string Base64Decode(string base64EncodedData)
         {
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            try
+            {
+                var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+                return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
         #endregion
 
